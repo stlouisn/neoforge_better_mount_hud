843001f583256b1718798bed16b2c66455f4ad2f
stlouisn <stlouisn@hotmail.com>
2024-09-08 10:22:32 -0300

    update to 1.21.1

---
+++ src/main/java/dev/bettermounthud/BetterMountHUD.java
@@ -0,0 +1,14 @@
+package dev.bettermounthud;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BetterMountHUD implements ModInitializer {
+
+  @SuppressWarnings("unused")
+  public static final Logger LOGGER = LoggerFactory.getLogger(Constants.MOD_ID);
+
+  public void onInitialize() {
+  }
+}
---
+++ src/main/java/dev/bettermounthud/Constants.java
@@ -0,0 +1,6 @@
+package dev.bettermounthud;
+
+public class Constants {
+
+  public static final String MOD_ID = "bettermounthud";
+}
